2a5608353bb5759a06f0f9f63124e8042c676e64
twhit027 <trewhite@gannett.com>
2019-03-26 10:27:38 -0400

    initial

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,37 @@
+import { Component, Prop } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+	tag: 'my-component',
+	styleUrls: [
+		'../../globals/variables.css',
+		'my-component.css'
+	],
+	shadow: true
+})
+export class MyComponent {
+	/**
+	* The first name
+	*/
+	@Prop() first: string;
+
+	/**
+	* The middle name
+	*/
+	@Prop() middle: string;
+
+	/**
+	* The last name
+	*/
+	@Prop() last: string;
+
+	private getText(): string {
+		return format(this.first, this.middle, this.last);
+	}
+
+	render() {
+		return (
+			<div class="blue">Hello, World! I'm {this.getText()}</div>
+		)
+	}
+}
